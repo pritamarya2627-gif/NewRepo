d1626bc142fbc13b478d769e6f13d001be86b86e
pritamarya2627-gif <pritamarya2627@gmail.com>
2025-12-02 13:03:52 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -32,8 +32,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
             InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
-        ],
-       
+        ],  
     ]
     
     return buttons
