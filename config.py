b0ad4a8cbb2ff9e526605de56c45fedfb1fc142e
pritamarya2627-gif <pritamarya2627@gmail.com>
2025-12-02 23:41:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 API_URL = getenv("API_URL", 'https://pytdbotapi.thequickearn.xyz') #youtube song url
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", "30DxNexGenBots46fb71")
+API_KEY = getenv("API_KEY", "Add_Api_key")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
